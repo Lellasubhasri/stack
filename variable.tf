b29610f3bf25e4a6c146d57ab5b168f82f724c46
lella subhasri <subhasri.lella@cloudreach.com>
2022-08-17 18:32:08 +0530

    vpc created with sg

---
+++ variable.tf
@@ -0,0 +1,41 @@
+variable "vpc_name" {
+  description = "this is the vpc name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "this is cidr address"
+
+}
+
+variable "aws_region" {
+  description = "resources will deploy here"
+  type        = string
+}
+
+variable "cidr_public" {
+  description = "this is for CIDR public subnet"
+}
+
+variable "cidr_private" {
+  description = "this is for CIDR private subnet"
+}
+
+variable "cidr_private1" {
+  description = "this is for CIDR private1 subnet"
+}
+
+# variable "ami_name" {
+#   description = " This is the owner name"
+#   type = string
+  
+# }
+
+
+variable "cidr_private2" {
+  description = "this is for CIDR private2 subnet"
+}
+
+variable "cidr_private3" {
+  description = "this is for CIDR private3 subnet"
+}
